821ff42b7195c1c35b83ffd70414e42a396619e6
Joe Linstrum <joelinstrum@Joes-MacBook-Pro.local>
2020-03-30 08:48:33 -0700

    Initial build commit

---
+++ precache-manifest.95205a9b58ce0aed5bebc5000ac34b5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb159b162c256a88e90b39c48f7d0a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fc8b255d88c029c7282",
+    "url": "/static/css/main.2275bbd9.chunk.css"
+  },
+  {
+    "revision": "3bba8a209233bc5a9d7c",
+    "url": "/static/js/2.4923a3e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4923a3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc8b255d88c029c7282",
+    "url": "/static/js/main.8928caa5.chunk.js"
+  },
+  {
+    "revision": "5474a125ae9567ce892a",
+    "url": "/static/js/runtime-main.b462ca10.js"
+  },
+  {
+    "revision": "31ec28230ca36de07e2bf59efe48de61",
+    "url": "/static/media/calculator.31ec2823.png"
+  }
+]);
